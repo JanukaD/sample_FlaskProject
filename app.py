372d70d068ad55c5a1231cb31fc6af7b6dabe09e
JanukaD <janukamadushan1995@gmail.com>
2019-08-28 11:33:47 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Januka Madushan',
+        'title': 'Post 01',
+        'content': 'Sample Post 01',
+        'date_posted': 'August 28,2019'
+    },
+    {
+        'author': 'Januka Madushan',
+        'title': 'Post 02',
+        'content': 'Sample Post 02',
+        'date_posted': 'August 28,2019'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
