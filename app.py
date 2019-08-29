cc1d1ba938ab0d73071c799e1ba7012e5ca89d4b
JanukaD <janukamadushan1995@gmail.com>
2019-08-29 14:08:52 +0530

    Create form validation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask , render_template, flash, redirect
 from flask import url_for
-from forms import RegistrarionForm, LoginForm
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
@@ -27,19 +27,26 @@ posts = [
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html',title="About")
+    return render_template('about.html', title="About")
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrarionForm()
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title="Register", form=form)
+
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title="Register", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
